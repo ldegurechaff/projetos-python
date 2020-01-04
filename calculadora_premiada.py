62281b933c24248778c6fe93b9f05189d37f758b
ldegurechaff <hypex21@gmail.com>
2020-01-04 04:27:27 -0300

    Uma simples calculadora em python com uma surpresinha rsrs

---
+++ calculadora_premiada.py
@@ -0,0 +1,62 @@
+import time, sys
+import winsound
+rel = time.ctime()
+
+for i in range(0, 5):
+    sys.stdout.write("\r{}".format(i))
+    sys.stdout.flush()
+    time.sleep(1)
+
+
+print("||||CALCULADORA PYTHON ||||", rel)
+
+print("\nDigite um operador aritmetico")
+op = str(input(""))
+if op == "+":
+    print("SOMA")
+    print("DIGITE OS VALORES")
+    print("N1 :")
+    n1 = int(input())
+    print("n2: ")
+    n2 = int(input())
+    total = n1 + n2
+    print("O total eh ")
+    print(total)
+    winsound.PlaySound("congratg.wav", winsound.SND_FILENAME)
+else :
+    if op == "-":
+        print("SUB")
+        print("DIGITE OS VALORES")
+        print("N1 :")
+        n1 = int(input())
+        print("n2: ")
+        n2 = int(input())
+        total = n1 - n2
+        print("O total eh ")
+        print(total)
+        winsound.PlaySound("teste.wav", winsound.SND_FILENAME)
+if op == "*":
+    if op == "-":
+        print("MULT")
+        print("DIGITE OS VALORES")
+        print("N1 :")
+        n1 = int(input())
+        print("n2: ")
+        n2 = int(input())
+        total = n1 * n2
+        print("O total eh ")
+        print(total)
+        winsound.PlaySound("teste.wav", winsound.SND_FILENAME)
+else :
+    if op == "/":
+            print("DIV")
+            print("DIGITE OS VALORES")
+            print("N1 :")
+            n1 = int(input())
+            print("n2: ")
+            n2 = int(input())
+            total = n1 / n2
+            print("O total eh ")
+            print(total)
+            winsound.PlaySound("teste.wav", winsound.SND_FILENAME)
+
